5aa4b73a568fd1b5ba80a61d8d2bef8962f24a06
ken7253 <ken7253@gmail.com>
2022-09-21 23:10:31 +0900

    Define init commands

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 import cac from 'cac';
+import { action } from './actions';
 
 const cli = cac();
 
+cli.command('init', 'Generate configuration file').action(() => {
+	action.init();
+});
 cli.help();
 cli.version('0.0.0');
