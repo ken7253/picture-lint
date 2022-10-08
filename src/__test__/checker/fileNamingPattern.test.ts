9a4f74ba763dfe9a899fbbe9d5af4fec89de3d34
ken7253 <ken7253@gmail.com>
2022-10-09 02:33:05 +0900

    Add checker "file-naming-pattern"

---
+++ src/__test__/checker/fileNamingPattern.test.ts
@@ -0,0 +1,28 @@
+import { fileNamingPattern } from '../../checker/fileNamingPattern';
+import { describe, expect, test } from '@jest/globals';
+
+describe('[checker: file-name-pattern]', () => {
+	// Rules allowing only "kebab-cases".
+	test('kebab-case', () => {
+		const kebabCase = 'root/foo/check-this-image.jpg';
+		const notKebabCase = 'root/foo/checkThisImage.jpg';
+		expect(fileNamingPattern(kebabCase, '^[a-z,0-9,-]+$')).toBe(true);
+		expect(fileNamingPattern(notKebabCase, '^[a-z,0-9,-]+$')).toBe(false);
+	});
+
+	// Rules requiring prefixing.
+	test('prefix', () => {
+		const prefix = 'root/foo/prefix-check-image.jpg';
+		const illegalPrefix = 'root/foo/check-image.jpg';
+		expect(fileNamingPattern(prefix, '^[prefix].*')).toBe(true);
+		expect(fileNamingPattern(illegalPrefix, '^[prefix].*')).toBe(false);
+	});
+
+	// Rules not including numbers.
+	test('ban number', () => {
+		const excludeNumber = 'root/foo/check-image.jpg';
+		const includeNumber = 'root/foo/check-image-01.jpg';
+		expect(fileNamingPattern(excludeNumber, '^(?!.*[0-9]).*$')).toBe(true);
+		expect(fileNamingPattern(includeNumber, '^(?!.*[0-9]).*$')).toBe(false);
+	});
+});
--- src/checker/fileNamingPattern.ts
+++ src/checker/fileNamingPattern.ts
@@ -1,3 +1,28 @@
-export const fileNamingPattern = () => {
-	//
+import path from 'node:path';
+
+/**
+ * Check image file naming conventions using RegExp
+ *
+ * @param filePath
+ *
+ * Target file path.
+ *
+ * ex) `root/foo/check-image.jpg`
+ *
+ * @param pattern
+ *
+ * check pattern.
+ *
+ * Enabled flags `'u'`
+ *
+ * @returns if failed test return `false`
+ */
+export const fileNamingPattern = (filePath: string, pattern: string) => {
+	const testPattern = new RegExp(pattern, 'u');
+	const { name } = path.parse(filePath);
+	const result = testPattern.test(name);
+
+	console.log(name, testPattern, result);
+
+	return result;
 };
