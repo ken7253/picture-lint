fc92d0c1b7b4f693574d42ea274eadc943304dd7
ken7253 <ken7253@gmail.com>
2022-10-14 23:09:45 +0900

    Change to async conifg and setting "cacheDirectory".

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,15 @@
 import type { Config } from 'jest';
+import path from 'node:path';
 
-const config: Config = {
-	roots: ['./src'],
-	testMatch: ['**/?(*.)+(spec|test).+(ts|js)'],
-	transform: {
-		'^.+\\.(ts|tsx)$': 'ts-jest',
-	},
+// eslint-disable-next-line @typescript-eslint/require-await
+export default async (): Promise<Config> => {
+	return {
+		cacheDirectory: path.join(process.cwd(), '.cache', 'jest'),
+		verbose: true,
+		roots: ['./src'],
+		testMatch: ['**/?(*.)+(spec|test).+(ts|js)'],
+		transform: {
+			'^.+\\.(ts|tsx)$': 'ts-jest',
+		},
+	};
 };
-
-export default config;
