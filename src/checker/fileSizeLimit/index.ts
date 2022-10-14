98234b1c0637bf2cb53a5006e50375eee44c659b
ken7253 <ken7253@gmail.com>
2022-10-14 22:41:32 +0900

    Describe JSDoc

--- src/checker/fileSizeLimit/index.ts
+++ src/checker/fileSizeLimit/index.ts
@@ -1,5 +1,14 @@
 import fs from 'node:fs/promises';
 
+/**
+ * Rules to check if the size of the image does not exceed the maximum size.
+ *
+ * @param filePath Path of the image to be checked.
+ *
+ * @param limit Maximum size [byte]
+ *
+ * @returns if "over-size" return `false`.
+ */
 export const fileSizeLimit = async (
 	filePath: string,
 	limit: number,
