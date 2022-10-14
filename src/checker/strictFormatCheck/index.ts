2d48a6ccec07dcd21a0b8332baba0e58420134b6
ken7253 <ken7253@gmail.com>
2022-10-15 02:15:09 +0900

    Define rules to check if the meta information and extension of an image match.

--- src/checker/strictFormatCheck/index.ts
+++ src/checker/strictFormatCheck/index.ts
@@ -1,3 +1,39 @@
-export const strictFormatCheck = () => {
-	// strictFormatCheck
+type ExtType = 'jpg' | 'png';
+
+export const strictFormatCheck = (meta: Uint8Array, is: ExtType) => {
+	if (meta.length < 16) {
+		throw new Error(
+			'Insufficient data size.\n Array length must be at least 16.',
+		);
+	}
+
+	const isPNG = () => {
+		const header = meta.slice(0, 8);
+		const correct = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
+		const testUint = Uint8Array.of(...correct);
+		const result = header.join() === testUint.join();
+		console.log(header, testUint);
+
+		return result;
+	};
+
+	const isJPG = () => {
+		const SOI = meta.slice(0, 2);
+		const correct = [0xff, 0xd8];
+		const testUint = Uint8Array.of(...correct);
+		const result = SOI.join() === testUint.join();
+
+		console.log(SOI, testUint);
+
+		return result;
+	};
+
+	switch (is) {
+		case 'jpg':
+			return isJPG();
+		case 'png':
+			return isPNG();
+		default:
+			throw new TypeError('Illegal argument.');
+	}
 };
