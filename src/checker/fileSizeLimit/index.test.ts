d289d2bc100e6f74ee97689bb884c7f59af21506
ken7253 <ken7253@gmail.com>
2022-10-14 22:26:59 +0900

    Create test case for "file-size-limit".

---
+++ src/checker/fileSizeLimit/index.test.ts
@@ -0,0 +1,31 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import process from 'node:process';
+import { fileSizeLimit } from './';
+import { describe } from '@jest/globals';
+
+const referenceImage = path.join(
+	process.cwd(),
+	'src',
+	'__test__',
+	'format-tester',
+	'jpg.jpg',
+);
+
+describe('[checker: file-size-limit]', () => {
+	test('over size', async () => {
+		const { size } = await fs.stat(referenceImage);
+		const result = await fileSizeLimit(referenceImage, size - 1);
+		expect(result).toBe(false);
+
+		return result;
+	});
+
+	test('safe size', async () => {
+		const { size } = await fs.stat(referenceImage);
+		const result = await fileSizeLimit(referenceImage, size + 1);
+		expect(result).toBe(true);
+
+		return result;
+	});
+});
