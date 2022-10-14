6b69e373170cbb43f883283074eb364243db8f71
ken7253 <ken7253@gmail.com>
2022-10-14 22:34:38 +0900

    Create function for "file-size-limit".

--- src/checker/fileSizeLimit/index.ts
+++ src/checker/fileSizeLimit/index.ts
@@ -1,3 +1,11 @@
-export const fileSizeLimit = () => {
-	//
+import fs from 'node:fs/promises';
+
+export const fileSizeLimit = async (
+	filePath: string,
+	limit: number,
+): Promise<boolean> => {
+	const { size } = await fs.stat(filePath);
+	const isSafeSize = size < limit;
+
+	return isSafeSize;
 };
