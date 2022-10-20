30084dac75494e13129c51da17624853144a07a6
ken7253 <ken7253@gmail.com>
2022-10-20 16:52:11 +0900

    WIP: create pretreatment object function.

--- src/pretreatment/create/index.ts
+++ src/pretreatment/create/index.ts
@@ -1,3 +1,28 @@
-export const create = () => {
+import path from 'node:path';
+import fs from 'node:fs/promises';
+import { Pretreatment, PretreatmentItem } from '..';
+
+export const create = async (
+	targetImagePathList: string[],
+): Promise<Pretreatment> => {
 	// pretreatment.create
+	const getPretreatment = async (
+		targetImagePath: string,
+	): Promise<PretreatmentItem> => {
+		const parsedPath = path.parse(targetImagePath);
+		const { size } = await fs.stat(targetImagePath);
+		const fileBinary = await fs.readFile(targetImagePath);
+		const header = new Uint8Array(fileBinary).slice(0, 16);
+
+		return {
+			path: parsedPath,
+			size,
+			header,
+		};
+	};
+
+	const result = Promise.all(
+		targetImagePathList.map((v) => getPretreatment(v)),
+	);
+	return result;
 };
--- src/pretreatment/index.ts
+++ src/pretreatment/index.ts
@@ -1,4 +1,13 @@
+import type { ParsedPath } from 'path';
 import { create } from './create';
+
+export interface PretreatmentItem {
+	path: ParsedPath;
+	header: Uint8Array;
+	size: number;
+}
+
+export type Pretreatment = PretreatmentItem[];
 
 export const pretreatment = {
 	create,
