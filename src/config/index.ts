56c83a8da88500325f7abf2c75f9e03cefb7c2c0
ken7253 <ken7253@gmail.com>
2022-10-23 22:54:48 +0900

    Add FILE_NAME static field

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,9 +2,12 @@ import { loader } from './loader/';
 import { defaultConfig, type Configuration } from './default/';
 import { generate } from './generate/';
 
+const FILE_NAME = 'picturelintrc.json';
+
 export type { Configuration };
 
 export const config = {
+	FILE_NAME,
 	defaultConfig,
 	loader,
 	generate,
