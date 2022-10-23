045469b4fb0904a6df264c32fb3ff5db0d0cebdb
ken7253 <ken7253@gmail.com>
2022-10-23 18:12:17 +0900

    Create generate config function.

--- src/config/generate/index.ts
+++ src/config/generate/index.ts
@@ -1,3 +1,13 @@
-export const generate = () => {
-	// config generator
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { defaultConfig } from '../default';
+
+export const generate = async () => {
+	const fileName = 'picturelintrc.json';
+	const targetPath = path.join(process.cwd(), fileName);
+	const config = JSON.stringify(defaultConfig);
+
+	await fs.writeFile(targetPath, config, {
+		encoding: 'utf-8',
+	});
 };
