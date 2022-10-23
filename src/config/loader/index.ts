61feb87dde456b733f3902bf403173047891bde3
ken7253 <ken7253@gmail.com>
2022-10-23 23:04:05 +0900

    Create config loader.

--- src/config/loader/index.ts
+++ src/config/loader/index.ts
@@ -1,3 +1,12 @@
-export const loader = () => {
-	// config loader
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { config, type Configuration } from '..';
+
+export const loader = async (): Promise<Configuration> => {
+	const configFilePath = path.join(process.cwd(), config.FILE_NAME);
+	const userRawConfig = await fs.readFile(configFilePath, { encoding: 'utf-8' });
+	const userConfig = JSON.parse(userRawConfig) as unknown;
+	const mixedConfig = config.marge(config.defaultConfig, userConfig);
+
+	return mixedConfig;
 };
