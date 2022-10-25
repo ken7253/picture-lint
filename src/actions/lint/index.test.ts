ff166e188ac280409895add6bd4257b72ea1c6cd
ken7253 <ken7253@gmail.com>
2022-10-25 22:30:00 +0900

    Create test code for "lint action".

---
+++ src/actions/lint/index.test.ts
@@ -0,0 +1,10 @@
+import { lint } from '.';
+import { config } from '../../config';
+import { describe } from '@jest/globals';
+
+describe('[actions: lint]', () => {
+	test('Error check', async () => {
+		const env = await config.loader();
+		expect(lint(env)).not.toThrow();
+	});
+});
