b30e72c9529c260e37cf81d9dbef684751b94841
ken7253 <ken7253@gmail.com>
2022-10-25 23:06:56 +0900

    WIP: Create lint action.

--- src/actions/lint/index.ts
+++ src/actions/lint/index.ts
@@ -1,3 +1,11 @@
-export const lint = () => {
-	// lint exec function
+import glob from 'glob';
+
+import type { Configuration } from '../../config';
+import { pretreatment } from '../../pretreatment';
+import { checker } from '../../checker';
+
+export const lint = (env: Configuration) => {
+	const { include } = env;
+	const r = include.map((pattern) => glob(pattern, (err, matches) => matches));
+	console.log(r);
 };
