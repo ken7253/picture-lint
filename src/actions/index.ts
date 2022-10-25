c1b47dad83529d7d70b931db76b208160b0bb9ee
ken7253 <ken7253@gmail.com>
2022-10-25 23:11:17 +0900

    Re-export lint action function,  define types.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,11 @@
+import type { Configuration } from '../config';
 import { init } from './init/';
+import { lint } from './lint';
 
-export const action = {
+type Action = (config: Configuration) => void;
+type Actions = { [key: string]: Action };
+
+export const action: Actions = {
 	init,
+	lint,
 };
