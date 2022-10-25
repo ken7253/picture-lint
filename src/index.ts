31e62a7d6e55ceb48850015763eed1c18f490c67
ken7253 <ken7253@gmail.com>
2022-10-25 23:08:01 +0900

    import config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 import cac from 'cac';
 import { action } from './actions';
+import { config } from './config';
 
 const cli = cac();
+const env = await config.loader();
 
 cli.command('init', 'Generate configuration file').action(() => {
-	action.init();
+	action.init(env);
 });
 cli.help();
 cli.version('0.0.0');
