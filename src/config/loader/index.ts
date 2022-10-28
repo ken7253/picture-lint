43bbe2dbaaa1e6d7871bed4e1645bd6c8aca43e4
ken7253 <ken7253@gmail.com>
2022-10-28 18:00:42 +0900

    Fixed to return default values if no configuration file can be found.

--- src/config/loader/index.ts
+++ src/config/loader/index.ts
@@ -4,8 +4,8 @@ import { config, type Configuration } from '..';
 
 export const loader = async (): Promise<Configuration> => {
 	const configFilePath = path.join(process.cwd(), config.FILE_NAME);
-	const userRawConfig = await fs.readFile(configFilePath, { encoding: 'utf-8' });
-	const userConfig = JSON.parse(userRawConfig) as unknown;
+	const userRawConfig = await fs.readFile(configFilePath, { encoding: 'utf-8' }).catch(() => null);
+	const userConfig = userRawConfig ? (JSON.parse(userRawConfig) as unknown) : null;
 	const mixedConfig = config.marge(config.defaultConfig, userConfig);
 
 	return mixedConfig;
