8a316a814e25e73550331c7a22d0cdc97f5bb806
ken7253 <ken7253@gmail.com>
2022-10-31 22:52:25 +0900

    Unify Checker types.

--- src/checker/index.ts
+++ src/checker/index.ts
@@ -2,7 +2,14 @@ import { strictFormatCheck } from './strictFormatCheck';
 import { fileNamingPattern } from './fileNamingPattern/';
 import { fileSizeLimit } from './fileSizeLimit/';
 
-export const checker = {
+import { Pretreatment } from '../pretreatment/';
+import { Configuration } from '../config/';
+
+type Checker = {
+	[key: string]: (target: Pretreatment, config: Configuration) => boolean;
+};
+
+export const checker: Checker = {
 	strictFormatCheck,
 	fileNamingPattern,
 	fileSizeLimit,
