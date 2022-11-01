462e5f0f50f35a4b9bce7624a14423b9949906c2
ken7253 <ken7253@gmail.com>
2022-11-01 23:29:17 +0900

    Fix type definitions.

--- src/checker/index.ts
+++ src/checker/index.ts
@@ -5,7 +5,7 @@ import { fileSizeLimit } from './fileSizeLimit/';
 import { PretreatmentItem } from '../pretreatment/';
 import { Configuration } from '../config/';
 
-export type Checker = (target: PretreatmentItem, config: Configuration) => boolean;
+export type Checker = (target: PretreatmentItem, config: Required<Configuration>) => boolean;
 
 export const checker: { [key: string]: Checker } = {
 	strictFormatCheck,
