86eef59cbd08eb1500387e9b711d36da066d9f7b
ken7253 <ken7253@gmail.com>
2022-11-20 03:05:57 +0900

    [WIP]: create lint feature.

--- src/actions/lint/index.ts
+++ src/actions/lint/index.ts
@@ -4,6 +4,12 @@ import type { Configuration } from '../../config';
 import { pretreatment } from '../../pretreatment';
 import { checker } from '../../checker';
 
-export const lint = (env: Configuration) => {
+export const lint = async (env: Configuration) => {
 	// lint
+	const globOption: glob.IOptions = { nodir: true, absolute: true };
+	const filePathList = env.include.map((v) => glob.sync(v, globOption));
+	const noDuplicates = Array.from(new Set(...filePathList));
+	/** @todo [WIP]: Pass to checker */
+	const pre = await pretreatment.create(noDuplicates);
+	const r = checker;
 };
