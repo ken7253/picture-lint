aff9d2aa0ede432bbf8dda0bae680ba5adef584e
ken7253 <ken7253@gmail.com>
2022-11-20 03:08:59 +0900

    use satisfies.

--- src/checker/index.ts
+++ src/checker/index.ts
@@ -7,8 +7,8 @@ import { Configuration } from '../config/';
 
 export type Checker = (target: PretreatmentItem, config: Required<Configuration>) => boolean;
 
-export const checker: { [key: string]: Checker } = {
+export const checker = {
 	strictFormatCheck,
 	fileNamingPattern,
 	fileSizeLimit,
-};
+} satisfies Record<string, Checker>;
