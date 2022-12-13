56b1ef7272872ed0a764818a69bcd7d3f202720f
ken7253 <ken7253@gmail.com>
2022-12-13 23:25:23 +0900

    Created to the point of receiving Checker results.

--- src/actions/lint/index.test.ts
+++ src/actions/lint/index.test.ts
@@ -5,6 +5,7 @@ import { describe } from '@jest/globals';
 describe('[actions: lint]', () => {
 	test('Error check', async () => {
 		const env = await config.loader();
+		env.include = ['src/__test__/**/*'];
 		expect(() => lint(env)).not.toThrow();
 	});
 });
