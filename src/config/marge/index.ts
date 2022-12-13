2ab6f7ea5b665f7c1fa3a2b0bfb7f109d397d5f3
ken7253 <ken7253@gmail.com>
2022-12-13 23:23:07 +0900

    Undo once the test has failed.

--- src/config/marge/index.ts
+++ src/config/marge/index.ts
@@ -12,13 +12,13 @@ import type { Configuration } from '..';
 export const marge = (main: Configuration, sub: unknown | Configuration): Configuration => {
 	if (sub === null || sub === undefined) return main;
 	const subInclude = Object.hasOwn(sub, 'include') ? (sub as { include: Configuration['include'] }).include : main.include;
-	const include = [...new Set([...subInclude, ...main.include])];
+	const include = [...new Set([...main.include, ...subInclude])];
 	const subRules = Object.hasOwn(sub, 'rules') ? (sub as { rules: Configuration['rules'] }).rules : main.rules;
 	const result: Configuration = {
 		include,
 		rules: {
-			...subRules,
 			...main.rules,
+			...subRules,
 		},
 	};
 
